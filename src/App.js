7e963dfb63b6597aabfffbc599b8ee3c1a266dc3
Natalie Childs <nata.faye@gmail.com>
2022-08-13 13:18:10 -0700

    8/13/22 class

--- src/App.js
+++ src/App.js
@@ -1,58 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+const fetchAllProducts = async () => {
+  const response = await fetch("http://localhost:3001/products")
+  const data = await response.json()
+  return data;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+  const [productList, setProductList] = useState([])
+
+  useEffect(() => {
+    const refreshProducts = async () => {
+      const freshProducts = await fetchAllProducts();
+      setProductList(freshProducts);
+    }
+    refreshProducts();
+  }, [])
+
+  const deleteProduct = (idToDelete) => {
+    // delete the product from the backend
+    fetch("http://localhost:3001/products/" + idToDelete, { method: "DELETE" })
+    // update the data on the frontend to match
+    setProductList(productList.filter(p => p.id !== idToDelete))
+  }
+
+  const createProduct = async (newProductData) => {
+    // create the product on the backend
+    const response = await fetch("http://localhost:3001/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProductData)
+    })
+    const dataFromServer = await response.json();
+
+    // update the data on the frontend to match
+    setProductList([...productList, dataFromServer])
+}
+
+return (
+  <div className="App">
+    <button className="btn btn-primary" onClick={() => createProduct({ name: "Steel", price: 50 })}>Create Steel</button>
+    <div>
+      {productList.map(product =>
+        <div key={product.id}>{product.name}<button onClick={() => deleteProduct(product.id)}>Delete</button></div>
+      )}
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
