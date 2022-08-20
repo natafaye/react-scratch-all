1d5a55a2e8c83f62b1b4bdafa0fc11a0d28f8919
Natalie Childs <nata.faye@gmail.com>
2022-08-20 13:07:02 -0700

    8/20/22

--- src/App.js
+++ src/App.js
@@ -1,57 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
+function Message(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="bg-primary text-white p-3">
+      { props.messageToShow }
     </div>
+  )
+}
+
+function App() {
+
+  const name = "Natalie"
+
+  const paragraph = <p>fdsfds { 5 + 3 }</p>
+
+  return (
+    <>
+      <p></p>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            { name }
+            { paragraph }
+            <Message messageToShow="Hi there!" />
+            <Message messageToShow="How are you?" />
+            <Message messageToShow="Goodbye!" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
