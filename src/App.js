b59872ec57fa62b2cf288cff69663a7dd5087127
Natalie Childs <nata.faye@gmail.com>
2023-07-01 20:27:59 -0700

    small changes

--- src/App.js
+++ src/App.js
@@ -1,113 +1,66 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect } from "react";
+import { useState } from "react";
+
+// const doSomethingSlowly = async () => {
+//     const response = await fetch("http://localhost:3004/products")
+//     const data = await response.json()
+//     return data
+// }
 
 export default function App() {
     const [productList, setProductList] = useState([])
-    const [errorMessage, setErrorMessage] = useState(null)
     const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        const getProducts = async () => {
+        const refreshProducts = async () => {
             setLoading(true)
-
-            let data;
-            let errorMessage = null
-            try {
-                const response = await fetch("http://localhost:3004/products")
-
-                if (!response.ok) {
-                    errorMessage = "Error: " + response.statusText
-                }
-
-                data = await response.json()
-            } catch (error) {
-                errorMessage = "Error: " + error.message
-            }
-
-            if(!errorMessage) {
-                setProductList(data)
-                setErrorMessage(null)
-                setLoading(false)
-            } else {
-                setErrorMessage(errorMessage)
-                setLoading(false)
-            }
-            
+            const response = await fetch("http://localhost:3004/products")
+            const freshProducts = await response.json()
+            setLoading(false)
+            setProductList(freshProducts)
         }
-        getProducts()
-    }, [])
+        refreshProducts()
+    }, []) // empty array = no reason to ever do this again
 
     const deleteProduct = async (idToDelete) => {
-        // PENDING
-        setLoading(true)
-
-        // THUNK
-        let errorMessage = null
         try {
-            const response = await fetch("http://localhost:3004/products/" + idToDelete, {
-                method: "DELETE"
-            })
+            // PENDING
+            setLoading(true)
+
+            // ASYNC THUNK
+            const response = await fetch("http://localhost:3004/products/" + idToDelete, { method: "DELETE" })
 
             if (!response.ok) {
-                errorMessage = "Error: " + response.statusText
+                // REJECTED
+                setError("Error: " + response.statusText)
+                setLoading(false)
+                return
             }
-        } catch (error) {
-            errorMessage = "Error: " + error.message
-        }
 
-        if(!errorMessage) {
             // FULFILLED
+            setLoading(false)
             setProductList(productList.filter(product => product.id !== idToDelete))
-            setErrorMessage(null)
-            setLoading(false)
-        } else {
+            setError(null)
+
+        } catch (error) {
             // REJECTED
-            setErrorMessage(errorMessage)
             setLoading(false)
+            setError("Error: " + error.message)
         }
-    }
-
-    const addProduct = (newProduct) => {
-        // Make the change on the backend
-        fetch("http://localhost:3004/products", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(newProduct)
-        })
-
-
-        // Make the change on the frontend
-        setProductList(productList.concat(newProduct))
-
-
-        if(!errorMessage) {
-            // Make the change on the frontend
-            setProductList(productList.concat(newProduct))
-            setErrorMessage(null)
-            setLoading(false)
-
-            // redirect to a submitted page
-            // set a piece of state that says that it was all good
-            // set a successful piece of state to true
-        } else {
-            setErrorMessage(errorMessage)
-            setLoading(false)
-        }
-
     }
 
     return (
-        <div className="m-5">
-            { errorMessage ? <p className="text-danger">{errorMessage}</p> : null }
-            { loading ? <p className="text-muted">Loading...</p> : null }
-            <ul className="list-group">
-                {productList.map(product => (
-                    <li className="list-group-item" key={product.id}>
+        <div>
+            {error ? <p className="text-danger">{error}</p> : null}
+            {loading ? <p>Loading...</p> : null}
+            <ul>
+                {productList.map(product =>
+                    <li key={product.id}>
                         {product.name}
-                        <button className="btn btn-danger" disabled={loading} onClick={() => deleteProduct(product.id)}>Delete</button>
+                        <button disabled={loading} onClick={() => deleteProduct(product.id)}>Delete</button>
                     </li>
-                ))}
+                )}
             </ul>
         </div>
     )
--- src/productsService.js
+++
@@ -1,5 +0,0 @@
-export const getAllProducts = async () => {
-    const response = await fetch("http://localhost:3001/products")
-    const data = await response.json() // unsmooshes the data
-    return data
-}
